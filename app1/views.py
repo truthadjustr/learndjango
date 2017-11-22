acdf774f78da97ae3b159e904373029adad697ad
Your Name <you@example.com>
2017-11-22 14:56:46 +0000

    pass variable into template

--- app1/views.py
+++ app1/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import datetime
+
 # Create your views here.
 def main(request):
-    return render(request,"main.html",{})
+    #today = datetime.datetime.now().date()
+    today = str(datetime.datetime.now())
+    return render(request,"main.html",{"today":today})
 
 def hello(request,param):
     return HttpResponse("<h2>hello %s</h2>" % ("stranger" if not param else param))
