50b9541f6decabb4a8ab38c82c73e5d823a41865
Your Name <you@example.com>
2017-11-22 14:46:44 +0000

    Use 'named capture group' in url regex

--- app1/urls.py
+++ app1/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url('^main/', views.main, name = 'main'),
     url('^hello/(\w+)?', views.hello, name = 'hello'),
+    url('^yyyymmdd/(?P<yyyy>\d{4})/(?P<mm>\d{2})/(?P<dd>\d{2})', views.yyyymmdd, name = 'yyyymmdd'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -7,3 +7,8 @@ def main(request):
 
 def hello(request,param):
     return HttpResponse("<h2>hello %s</h2>" % ("stranger" if not param else param))
+
+def yyyymmdd(request,yyyy,mm,dd):
+    # url params are received as string
+    x = "%s/%s/%s" % (yyyy,mm,dd)
+    return HttpResponse("<h3>Schedule: %s</h3>" % x)
